1a5f64e24a009eee1018aef805ceaa51a49b1a33
keo <380213949@qq.com>
2019-08-06 01:45:28 +0800

    422bug

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
-const appID = "yLG8dfNvwBVFr3td6wHCSUix";
-const appSecret = "WembHPWGgcq8V9Swpv3Y7RZp" ;
+
+const appID = "yLG8dfNvwBVFr3td6wHCSUix"
+const appSecret = "WembHPWGgcq8V9Swpv3Y7RZp"
+
+
 
 const instance = axios.create({
     baseURL: 'https://gp-server.hunger-valley.com/',
@@ -12,33 +15,27 @@ const instance = axios.create({
 });
 
 // Add a request interceptor
-// tslint:disable-next-line:only-arrow-functions
-instance.interceptors.request.use(function (config) {
+instance.interceptors.request.use( (config)=> {
     const xToken = localStorage.getItem('x-token')
     if(xToken){
-        config.headers.Authorization = `Bearer ${xToken}`
+        config.headers['Authorization'] = `Bearer ${xToken}`
     }
     return config;
-// tslint:disable-next-line:only-arrow-functions
-}, function (error) {
-    // tslint:disable-next-line:no-console
+},  (error)=> {
     console.error(error)
     return Promise.reject(error);
 });
 
 // Add a response interceptor
-// tslint:disable-next-line:only-arrow-functions
-instance.interceptors.response.use(function (response) {
+instance.interceptors.response.use( (response)=> {
     // Do something with response data
     if(response.headers['x-token']){
         localStorage.setItem('x-token',response.headers['x-token'])
     }
     return response;
-// tslint:disable-next-line:only-arrow-functions
-}, function (error) {
+},  (error)=> {
     // Do something with response error
     return Promise.reject(error);
 });
 
 export default instance
-
