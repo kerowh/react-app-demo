2f1c496d6e5b3a94a5297bee70fcf91f846e581f
keo <380213949@qq.com>
2019-08-06 12:47:17 +0800

    完成注册页面

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,41 +1,45 @@
 import axios from 'axios';
-
+import history from './history'
 
 const appID = "yLG8dfNvwBVFr3td6wHCSUix"
 const appSecret = "WembHPWGgcq8V9Swpv3Y7RZp"
 
-
-
+/* tslint:disable:no-string-literal */
 const instance = axios.create({
-    baseURL: 'https://gp-server.hunger-valley.com/',
-    headers: {
-        't-app-id': appID,
-        't-app-secret': appSecret
-    }
+	baseURL: 'https://gp-server.hunger-valley.com/',
+	headers: {
+		't-app-id': appID,
+		't-app-secret': appSecret
+	}
 });
 
 // Add a request interceptor
-instance.interceptors.request.use( (config)=> {
-    const xToken = localStorage.getItem('x-token')
-    if(xToken){
-        config.headers['Authorization'] = `Bearer ${xToken}`
-    }
-    return config;
-},  (error)=> {
-    console.error(error)
-    return Promise.reject(error);
+instance.interceptors.request.use((config) => {
+	const xToken = localStorage.getItem('x-token')
+	if(xToken){
+		config.headers['Authorization'] = `Bearer ${xToken}`
+	}
+	return config;
+},  (error) => {
+	console.error(error)
+	return Promise.reject(error);
 });
 
 // Add a response interceptor
-instance.interceptors.response.use( (response)=> {
-    // Do something with response data
-    if(response.headers['x-token']){
-        localStorage.setItem('x-token',response.headers['x-token'])
-    }
-    return response;
-},  (error)=> {
-    // Do something with response error
-    return Promise.reject(error);
+instance.interceptors.response.use((response) => {
+	// Do something with response data
+	if(response.headers['x-token']){
+		localStorage.setItem('x-token',response.headers['x-token'])
+	}
+	return response;
+},  (error) => {
+	if(error.response.status === 401){
+		console.log("重定向");
+		history.push('/login')
+	}
+	// Do something with response error
+	return Promise.reject(error);
 });
 
+/* tslint:enable:no-string-literal */
 export default instance
