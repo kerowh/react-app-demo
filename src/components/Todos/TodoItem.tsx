b01179682f3dcc99744061d51d3dcac8b9a87a17
keo <380213949@qq.com>
2019-08-08 16:15:28 +0800

    完成todoitem组件

---
+++ src/components/Todos/TodoItem.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import {Checkbox,Icon} from "antd";
+
+interface ITodoItemProps {
+    id: number;
+    description: string;
+    completed: boolean;
+    editing:boolean
+    update: (id: number, params: any)=> void;
+    toEditing: (id: number)=> void;
+}
+
+
+interface ITodoItemState {
+    editText:string
+}
+
+class TodoItem extends React.Component<ITodoItemProps,ITodoItemState> {
+    constructor(props){
+        super(props)
+        this.state ={
+            editText:this.props.description
+        }
+    }
+
+    update = (params:any) => {
+        this.props.update(this.props.id,params)
+    }
+
+    toEditing =()=>{
+        this.props.toEditing(this.props.id)
+    }
+
+    onKeyUp = (e)=>{
+        if (e.keyCode === 13 && this.state.editText !== ''){
+            this.update({description: this.state.editText})
+        }
+    }
+
+    public render() {
+        const Editing =(
+            <div className="editing">
+                {/*这里使用state中的数据是因为，props传过来的是参数，*/}
+                {/*state中才是你本身的，*/}
+                {/*你只能修改你本身的参数，props中的你只能获取*/}
+                <input type="text" value={this.state.editText}
+                        onChange={e => this.setState({editText:e.target.value})}
+                        onKeyUp={this.onKeyUp}
+                />
+                <div className="iconWrapper">
+                    <Icon type="enter" />
+                    <Icon type="delete" theme="filled"
+                          onClick={e=> this.update({deleted:true})}
+                    />
+                </div>
+            </div>
+        )
+
+        const Text = <span onDoubleClick={this.toEditing}>{this.props.description}</span>
+
+        return (
+            <div className="TodoItem" id="TodoItem">
+                <Checkbox checked={this.props.completed}
+                          onChange={e=> this.update({completed: e.target.checked})}
+                />
+                {this.props.editing?Editing:Text}
+            </div>
+        );
+    }
+}
+
+export default TodoItem;
--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import TodoInput from 'src/components/Todos/TodoInput'
+import TodoItem from 'src/components/Todos/TodoItem'
 import axios from 'src/config/axios'
 import './Todo.scss'
 
@@ -18,7 +19,6 @@ class Todos extends React.Component<any,ITodosState> {
         const {todos} = this.state
         try {
             const response = await axios.post('todos',params)
-            console.log(response.data);
             this.setState({todos:[response.data.resource,...todos]})
         }catch (e) {
             throw new Error(e)
@@ -32,11 +32,41 @@ class Todos extends React.Component<any,ITodosState> {
     getTodos =async ()=>{
         try {
             const response = await axios.get('todos')
-            console.log(response);
-            this.setState({todos:response.data.resources})
+            const todos = response.data.resources.map(t =>Object.assign({},t,{editing:false}))
+            // es6:todos:todos === {todos}
+            this.setState({todos})
         }catch (e) {
             throw new Error(e)
         }
+    }
+
+    updateTodo = async (id:number,params:any) => {
+        const {todos} = this.state
+        try {
+            const response = await axios.put(`todos/${id}`, params)
+            const newTodos = todos.map(t => {
+                if (id === t.id) {
+                    return response.data.resource
+                } else {
+                    return t
+                }
+            })
+            this.setState({todos: newTodos})
+        } catch (e) {
+            throw new Error(e)
+        }
+    }
+
+    toEditing =(id:number)=>{
+        const {todos} =this.state
+        const newTodos = todos.map(t=>{
+            if (id===t.id){
+                return Object.assign({},t,{editing:true})
+            }else {
+                return Object.assign({},t,{editing:false})
+            }
+        })
+        this.setState({todos:newTodos})
     }
 
     public render() {
@@ -46,9 +76,9 @@ class Todos extends React.Component<any,ITodosState> {
                 <TodoInput addTodo={(params)=>this.addTodo(params)}></TodoInput>
                 <main>
                     {
-                        this.state.todos.map(t =>{
-                            return <div key={t.id}>{t.description}</div>
-                        })
+                        this.state.todos.map(t =><TodoItem key={t.id} {...t}
+                                                           update={this.updateTodo} toEditing={this.toEditing}
+                        />)
                     }
                 </main>
             </div>
